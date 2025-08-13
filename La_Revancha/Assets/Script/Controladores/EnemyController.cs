f3afb8066bee957d5bc2103f1010a21031ecad0d
Diego-Vivanco-98 <diegovqd@gmail.com>
2025-08-13 07:46:32 -0600

    Enemigos: Configuración de Patrullaje de enemigo.
    Se crea el script para el control del enemigo así como del animator controller.

--- La_Revancha/Assets/Script/Controladores/EnemyController.cs
+++ La_Revancha/Assets/Script/Controladores/EnemyController.cs
@@ -1,21 +1,66 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
+    public Vector2 patrolArea;
+    public float patrolRefreshTime;
     private NavMeshAgent agent;
     private GameObject player;
+    private Animator animator;
+    private Vector3 destination;
+
+    private EnemyState currentState;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         player = GameObject.FindWithTag("prota");
+        animator = GetComponent<Animator>();
+        ChangeState(EnemyState.PATROL);
     }
 
     // Update is called once per frame
     void Update()
     {
-        agent.SetDestination(player.transform.position);
+        //agent.SetDestination(player.transform.position);
+        agent.SetDestination(destination);
+        animator.SetFloat("VelX", agent.velocity.sqrMagnitude);
     }
+
+    public void ChangeState(EnemyState newState)
+    {
+        switch (newState)
+        {
+            case EnemyState.PATROL:
+                StartCoroutine("WaitToNewPatrolPoint");
+                break;
+            case EnemyState.CHASE:
+                destination = player.transform.position;
+                break;
+            case EnemyState.ATACK:
+                break;
+        }
+        currentState = newState;
+
+    }
+
+    IEnumerator WaitToNewPatrolPoint()
+    {
+        while (currentState == EnemyState.PATROL)
+        {
+            destination = transform.position + new Vector3(Random.Range(-patrolArea.x, patrolArea.x), 0f, Random.Range(-patrolArea.y, patrolArea.y));
+            yield return new WaitForSeconds(patrolRefreshTime);
+        }
+
+    }
+}
+
+public enum EnemyState
+{
+    PATROL,
+    CHASE,
+    ATACK
 }
