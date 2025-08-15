ac2eae91eb65569ed2b1a8e31ebb79e70e9b6142
Diego-Vivanco-98 <diegovqd@gmail.com>
2025-08-15 09:19:53 -0600

    Shaders: Se agregan materiales especiales para Items y Portal
    Se crean algunos shaders para efectos especiales sobre items y se crea un efecto de portal básico.

--- La_Revancha/Assets/Script/Items/ItemRotation.cs
+++ La_Revancha/Assets/Script/Items/ItemRotation.cs
@@ -1,19 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class ItemRotation : MonoBehaviour
 {
-    public float speedRot = 25.0f;
+    public float speedRotZ;
+    public float speedRotY;
     // Start is called before the first frame update
     void Start()
-    {
+    {   
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 0, Time.deltaTime * speedRot);
+        transform.Rotate(0, 0, Time.deltaTime * speedRotZ);
+        transform.Rotate(0, Time.deltaTime * speedRotY, 0);
     }
 }
