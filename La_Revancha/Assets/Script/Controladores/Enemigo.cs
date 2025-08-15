f5c2a34de551ff27cecedb15eac71c36620c6ed9
Diego-Vivanco-98 <diegovqd@gmail.com>
2025-08-14 22:55:25 -0600

    Enemigos: Se implementa ataque del enemigo.
    Se agrega el estado de ataque con su animación correspondiente.

--- La_Revancha/Assets/Script/Controladores/Enemigo.cs
+++ La_Revancha/Assets/Script/Controladores/Enemigo.cs
@@ -1,80 +1,137 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class Enemigo : MonoBehaviour
 {
     public Vector2 patrolArea;
-    public float patrolRefreshTime;
-    private NavMeshAgent agent;
-    private GameObject player;
+    public float patrolTime;
+    public float chaseDistance;
+
     private Animator animator;
-    private EnemyState currentState;
-    private Vector3 destination;
-    private Ray enemyRay;
-    private RaycastHit hit;
-    
+    private NavMeshAgent navMeshAgent;
+    private Transform playerTransform;
+    private Vector3 playerPosition;
+    private EnemyState enemyState;
+    private Vector3 patrolDestination;
 
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+   // public GameObject barraVida;
+    //public int vida = 100;
     void Start()
     {
-        agent = GetComponent<NavMeshAgent>();
-        player = GameObject.FindWithTag("prota");
         animator = GetComponent<Animator>();
-        ChangeState(EnemyState.PATROL);
-        //currentState = GetComponent<EnemyState>();
-       
-        
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        playerTransform = GameObject.FindGameObjectWithTag("prota").transform;
+        enemyState = EnemyState.PATROL;
+        InvokeRepeating("CalculatePatrolDestination", 0f, patrolTime);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        Debug.DrawRay(transform.position, transform.forward * 100f, Color.red);
-        agent.SetDestination(destination);
-        animator.SetFloat("speed", agent.velocity.sqrMagnitude);
-
-        enemyRay = new Ray(transform.position + Vector3.up, transform.forward);
-
-        
-        if (Physics.SphereCast(enemyRay, 2f, out hit, 20f))
-        {
-            if (hit.transform.CompareTag("prota"))
-            {
-                Debug.Log("Detecta al jugador");
-                ChangeState(EnemyState.CHASE);
-            }
-        }
-
-        
-        //ChangeState(EnemyState.PATROL);
-    }
-    public void ChangeState(EnemyState newState)
-    {
-        switch (newState)
+        switch (enemyState)
         {
             case EnemyState.PATROL:
-                StartCoroutine("WaitToNewPatrolPoint");
-                Debug.Log("El enemigo esta patrullando");
+                /*if (Vector3.Distance(transform.position, playerTransform.position) <= chaseDistance)
+                    enemyState = EnemyState.CHASE;
+                else
+                    navMeshAgent.SetDestination(patrolDestination);*/
+                Patrol();
                 break;
             case EnemyState.CHASE:
-                destination = player.transform.position;
+                Chase();
+                /*navMeshAgent.SetDestination(playerTransform.position);
+                animator.SetFloat("Speed", navMeshAgent.velocity.sqrMagnitude);*/
                 break;
-            case EnemyState.ATACK:
+            case EnemyState.ATTACK:
+                Atack();
+                /*if (Vector3.Distance(transform.position, playerTransform.position) >= chaseDistance)
+                    animator.SetTrigger("Ataque");*/
                 break;
         }
-        currentState = newState;
     }
 
-    IEnumerator WaitToNewPatrolPoint()
+    public void CalculatePatrolDestination()
     {
-        while (currentState == EnemyState.PATROL)
-        {
-            destination = transform.position + new Vector3(Random.Range(-patrolArea.x, patrolArea.x), 0f, Random.Range(-patrolArea.y, patrolArea.y));
-            yield return new WaitForSeconds(patrolRefreshTime);
-        }
+        patrolDestination = transform.position + new Vector3(Random.Range(-patrolArea.x, patrolArea.x),
+                                                            0f,
+                                                            Random.Range(-patrolArea.y, patrolArea.y));
+    }
 
+    /*
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Item"))
+        {
+            Debug.Log("Contacto con barra");
+            PerderVidaEnemigo();
+        }
+    }
+    private void PerderVidaEnemigo()
+    {
+        vida = vida - 50;
+        barraVida.GetComponent<Slider>().value = vida;
+        RevisarVida();
+    }
+
+    private void RevisarVida()
+    {
+        if (vida == 0)
+        {
+            animator.SetTrigger("Morir");
+            StartCoroutine(TiempoMuerte());
+
+        }
+    }
+
+    private IEnumerator TiempoMuerte()
+    {
+        yield return new WaitForSeconds(2f);
+        Destroy(gameObject);
+    }
+    */
+    private void Patrol()
+    {
+        if (Vector3.Distance(transform.position, playerTransform.position) <= chaseDistance)
+        {
+            enemyState = EnemyState.CHASE;
+        }
+        else
+        {
+            navMeshAgent.SetDestination(patrolDestination);
+            animator.SetFloat("walk", navMeshAgent.velocity.sqrMagnitude);
+        }
+    }
+
+    private void Chase()
+    {
+        if (Vector3.Distance(transform.position, playerTransform.position) > chaseDistance)
+        {
+            enemyState = EnemyState.PATROL;
+        }
+        else
+        {
+            if (Vector3.Distance(transform.position, playerTransform.position) <= 5f)
+            {
+                enemyState = EnemyState.ATTACK;
+            }
+            else
+            {
+                navMeshAgent.SetDestination(playerTransform.position);
+                animator.SetFloat("speed", navMeshAgent.velocity.sqrMagnitude);
+            }
+        }
+    }
+
+    private void Atack()
+    {
+        navMeshAgent.SetDestination(playerTransform.position);
+        animator.SetTrigger("AtaqueEnemigo");
+        if (Vector3.Distance(transform.position, playerTransform.position) > 5f)
+        {
+            enemyState = EnemyState.CHASE;
+        }
     }
 }
 
@@ -82,5 +139,7 @@ public enum EnemyState
 {
     PATROL,
     CHASE,
-    ATACK
+    ATTACK
 }
+
+
